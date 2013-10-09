e9a770018a73551a2383d6b3d6131ebf7a7398ce
Rob Day <rkd@rkd.me.uk>
2013-10-09 08:39:09 +0100

    SDP example

---
+++ uac-tcp-body.rb
@@ -0,0 +1,20 @@
+require 'quaff'
+require 'sdp'
+
+phone = TCPSIPEndpoint.new(5061)
+call = phone.new_call("1",
+                      "sip:uac@example.com",
+                      phone.new_connection('localhost', 5060),
+                      "sip:uas@example.com")
+
+call.send_request("INVITE", "some sdp", {"Content-Type" => "text/plain"})
+call.recv_response("180")
+ans = call.recv_response("200")
+sdp = SDP.parse ans['message'].body
+puts sdp.inspect
+call.send_request("ACK")
+sleep 2
+call.send_request("BYE")
+call.recv_response("200")
+puts "Successful call!"
+call.end_call
