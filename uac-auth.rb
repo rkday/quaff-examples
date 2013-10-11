7fae5956c13b72ba2ac4b727dfdf76063ab35a1e
Rob Day <rkd@rkd.me.uk>
2013-10-11 14:29:20 +0100

    Add authentication example, make filenames more consistent

---
+++ uac-auth.rb
@@ -0,0 +1,18 @@
+require 'quaff'
+
+phone = Quaff::UDPSIPEndpoint.new(5061)
+call = phone.new_call("1",
+                      "sip:uac@example.com",
+                      phone.new_connection('localhost', 5060),
+                      "sip:uas@example.com")
+
+call.register("myself", "secret")
+call.send_request("INVITE")
+call.recv_response("180")
+call.recv_response("200")
+call.send_request("ACK")
+sleep 2
+call.send_request("BYE")
+call.recv_response("200")
+puts "Successful call!"
+call.end_call
